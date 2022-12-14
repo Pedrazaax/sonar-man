677c42fd9b8aa4d303bf255362b7d2c62a35054c
Pedrazaax <carlos.pedraza@alu.uclm.es>
2022-12-14 19:48:42 +0100

    test solucionados

--- src/test/java/com/TIComoApp/TIComo/AuthenticationControllerTest.java
+++ src/test/java/com/TIComoApp/TIComo/AuthenticationControllerTest.java
@@ -5,10 +5,11 @@ import org.bson.json.JsonObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.TIComoApp.TIComo.model.Administrador;
 import com.TIComoApp.TIComo.model.Cliente;
 import com.TIComoApp.TIComo.controller.AuthenticationController;
 import com.TIComoApp.TIComo.controller.ClienteController;
-
 
 @SpringBootTest
 class AuthenticationControllerTest {
@@ -17,40 +18,132 @@ class AuthenticationControllerTest {
 	@Autowired
 	private ClienteController clicontroller;
 
-	
-//	@Test
-//	void createTest() {
-//		Cliente cliente= new Cliente("10","Paco","PEPE","pacopepe@gmail.com", "12345678Ab","Norte","Calle","666666666");//cambiar a uno que no existe
-//		Cliente cliente2= new Cliente("10ADSD","Paco","PEPE","paco@gmail.com", "12345678Ab","Norte","Calle3","784");
-//		Cliente cliente3= new Cliente("10SCASD","Paco","PEPE","pacopepe@gmail.com", "12345","Norte","Calle","987987987");
-//		Cliente cliente4= new Cliente("10as","Paco","PEPE","paco@gmail.com", "12345678Ab","Norte","Calle 2","784857254");
-//       autcontroller.create(cliente);
-//       autcontroller.create(cliente2);
-//       autcontroller.create(cliente3);
-//       autcontroller.create(cliente4);
-//       
-//
-//	}
-//	
-//	@Test
-//	void loginTest() {
-//		
-//		JsonObject jadmin=autcontroller.loginUser("javieradmin@ticomo.com","44444444Zz");
-//		JsonObject jcliente=autcontroller.loginUser("julianmuñoz@gmail.com","77777777Vv");
-//		JsonObject jrider=autcontroller.loginUser("joseluisrider@ticomo.com","12345678Aa");
-//		assertEquals("{\"respuesta\":\"adminLogin\"",jadmin.toString());
-//		assertEquals("{\"respuesta\":\"clienteLogin\",\"idCliente\":\"63624df247774a5c1af31a30\"}",jcliente.toString());
-//		assertEquals("{\"respuesta\":\"riderLogin\",\"idRider\":\"63624be447774a5c1af31a2c\"}",jrider.toString());
-//		
-//		JsonObject jadmin2=autcontroller.loginUser("javieradmin@ticomo.com","44444444Zzz");
-//		JsonObject jcliente2=autcontroller.loginUser("julianmuñoz@gmail.com","77777777Vz");
-//		JsonObject jrider2=autcontroller.loginUser("joseluisrider@ticomo.com","12345678Aaa");
-//		assertEquals("{\"respuesta\":\"Email o Password  incorrecto\"}",jadmin2.toString());
-//		assertEquals("{\"respuesta\":\"Email o Password incorrectos\"}",jcliente2.toString());
-//		assertEquals("{\"respuesta\":\"Email o Password incorrecto\"}",jrider2.toString());
-//		
-//		autcontroller.loginUser("javieradmixcsdcsdn@ticomo.com","444");
-//
-//	}
+	@Test
+	void createTest() throws Exception {
+		Cliente cliente = new Cliente("Paco", "PEPE", "paco@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente cliente2 = new Cliente("", "PEPE", "pacopepe@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente cliente3 = new Cliente("Paco", "PEPE", "pacopepe@gmail", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente cliente4 = new Cliente("Paco", "PEPE", "pacopepe@gmail.com", "8aracteres", "8aracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente cliente5 = new Cliente("Paco", "PEPE", "pacopepe@gmail.com", "8Caracteres", "8Caractere", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente cliente6 = new Cliente("Paco", "PEPE", "pacopepe@gmail.com", "8Caracteres", "8Caractere", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente cliente7 = new Cliente("Paco", "PEPE", "pacopepe@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678", "Dirección", "666666666");
+		Cliente cliente8 = new Cliente("Paco", "PEPE", "pacopepe@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "166666666");
+
+		try {
+			autcontroller.create(cliente);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			autcontroller.create(cliente2);
+			
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			autcontroller.create(cliente3);
+			
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			autcontroller.create(cliente4);
+			
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			autcontroller.create(cliente5);
+			
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			autcontroller.create(cliente6);
+			
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			autcontroller.create(cliente7);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			autcontroller.create(cliente8);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+
+	}
+
+	@Test
+	void loginTest() throws Exception {
+
+		Cliente admin = new Cliente("", "", "admin@admin.com", "8Caracteres", "8Caracteres", true, 5, "", "", ""); //No es valido
+		Cliente cliente = new Cliente("", "", "manolin@elgafotilla.com", "8Caracteres", "8Caracteres", true, 5, "", "",
+				""); //No es valido
+		Cliente rider = new Cliente("", "", "pruebitaridder@gmail.com", "8Caracteres", "8Caracteres", true, 5, "", "",
+				"");
+		Cliente asis = new Cliente("", "", "mario@mario.com", "8Caracteres", "8Caracteres", true, 5, "", "", "");
+
+		Cliente admin2 = new Cliente("", "", "admin@admin.com", "8Caractere", "8Caracteres", true, 5, "", "", "");
+		Cliente cliente2 = new Cliente("", "", "manolin@elgafotilla.com", "8Caractere", "8Caracteres", true, 5, "", "",
+				"");
+		Cliente rider2 = new Cliente("", "", "pruebitaridder@gmail.com", "8Caractere", "8Caracteres", true, 5, "", "",
+				"");
+		Cliente asis2 = new Cliente("", "", "mario@mario.com", "8Caractere", "8Caracteres", true, 5, "", "", "");
+		
+		try {
+			String jadmin = autcontroller.loginUser(admin);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			String jcliente = autcontroller.loginUser(cliente);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			String jrider = autcontroller.loginUser(rider);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			String jasis = autcontroller.loginUser(asis);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		
+		try {
+			String jadmin2 = autcontroller.loginUser(admin2);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			String jcliente2 = autcontroller.loginUser(cliente2);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			String jrider2 = autcontroller.loginUser(rider2);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		try {
+			String jasis2 = autcontroller.loginUser(asis2);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+
+	}
 
 }
--- src/test/java/com/TIComoApp/TIComo/ClienteTest.java
+++ src/test/java/com/TIComoApp/TIComo/ClienteTest.java
@@ -12,162 +12,179 @@ import com.TIComoApp.TIComo.model.Administrador;
 import com.TIComoApp.TIComo.model.Cliente;
 
  class ClienteTest {
-//	@Test
-//	void testCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getId(),ejemplo2.getId());
-//		assertEquals(ejemplo.getNombre(),ejemplo2.getNombre());
-//		assertEquals(ejemplo.getApellidos(),ejemplo2.getApellidos());
-//		assertEquals(ejemplo.getEmail(),ejemplo2.getEmail());
-//		assertEquals(ejemplo.getPassword(),ejemplo2.getPassword());
-//		assertEquals(ejemplo.getNIF(),ejemplo2.getNIF());
-//		assertEquals(ejemplo.getDireccionCompleta(),ejemplo2.getDireccionCompleta());
-//		assertEquals(ejemplo.getTelefono(),ejemplo2.getTelefono());	
-//	}
-//
-//	@Test
-//	void testGetNIFCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getNIF(),ejemplo2.getNIF());
-//	}
-//
-//	@Test
-//	void testSetNIFCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		ejemplo.setNIF("98215238B");
-//		ejemplo2.setNIF("98215238B");
-//		assertEquals(ejemplo.getNIF(),ejemplo2.getNIF());
-//	}
-//
-//	@Test
-//	void testGetDireccionCompletaCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getDireccionCompleta(),ejemplo2.getDireccionCompleta());
-//	}
-//
-//	@Test
-//	void testSetDireccionCompletaCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		ejemplo.setDireccionCompleta("Juan Avila 12");
-//		ejemplo2.setDireccionCompleta("Juan Avila 12");
-//		assertEquals(ejemplo.getDireccionCompleta(),ejemplo2.getDireccionCompleta());
-//	}
-//
-//	@Test
-//	void testGetTelefonoCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getTelefono(),ejemplo2.getTelefono());
-//	}
-//
-//	@Test
-//	void testSetTelefonoCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		ejemplo.setTelefono("817291863");
-//		ejemplo2.setTelefono("817291863");
-//		assertEquals(ejemplo.getTelefono(),ejemplo2.getTelefono());
-//	}
-//
-//	@Test
-//	void testGetIdCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getId(),ejemplo2.getId());
-//	}
-//
-//	@Test
-//	void testSetIdCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		ejemplo.setId("3");
-//		ejemplo2.setId("3");
-//		assertEquals(ejemplo.getId(),ejemplo2.getId());
-//	}
-//
-//	@Test
-//	void testGetNombreCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getNombre(),ejemplo2.getNombre());
-//	}
-//
-//	@Test
-//	void testSetNombreCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		ejemplo.setNombre("Leandro");
-//		ejemplo2.setNombre("Leandro");
-//		assertEquals(ejemplo.getNombre(),ejemplo2.getNombre());
-//	}
-//
-//	@Test
-//	void testGetApellidosCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getApellidos(),ejemplo2.getApellidos());
-//	}
-//
-//	@Test
-//	void testSetApellidosCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		ejemplo.setApellidos("Fernandez");
-//		ejemplo2.setApellidos("Fernandez");
-//		assertEquals(ejemplo.getApellidos(),ejemplo2.getApellidos());
-//	}
-//
-//	@Test
-//	void testGetEmailCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getEmail(),ejemplo2.getEmail());
-//	}
-//
-//	@Test
-//	void testSetEmailCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		ejemplo.setEmail("LFernandez@gmail.com");
-//		ejemplo2.setEmail("LFernandez@gmail.com");
-//		assertEquals(ejemplo.getEmail(),ejemplo2.getEmail());
-//	}
-//
-//	@Test
-//	void testGetPasswordCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.getPassword(),ejemplo2.getPassword());
-//	}
-//
-//	@Test
-//	void testSetPasswordCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		ejemplo.setPassword("12445178Aa");
-//		ejemplo2.setPassword("12445178Aa");
-//		assertEquals(ejemplo.getPassword(),ejemplo2.getPassword());
-//	}
-//	
-//	@Test
-//	void testtoStringCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.toString(),ejemplo2.toString());
-//		
-//	}
-//	
-//	@Test
-//	void testContraseniaSeguraCliente() {
-//		Cliente ejemplo=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		Cliente ejemplo2=new Cliente("1","Facundo","Corral","FCorral@gmail.com","12345678Aa","98215698B","Bailen 12", "734523423");
-//		assertEquals(ejemplo.contraseniaSegura(ejemplo.getPassword()),ejemplo2.contraseniaSegura(ejemplo2.getPassword()));
-//	
-//	}
+	@Test
+	void testCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.getNombre(),ejemplo2.getNombre());
+		assertEquals(ejemplo.getApellidos(),ejemplo2.getApellidos());
+		assertEquals(ejemplo.getEmail(),ejemplo2.getEmail());
+		assertEquals(ejemplo.getPassword(),ejemplo2.getPassword());
+		assertEquals(ejemplo.getNIF(),ejemplo2.getNIF());
+		assertEquals(ejemplo.getDireccionCompleta(),ejemplo2.getDireccionCompleta());
+		assertEquals(ejemplo.getTelefono(),ejemplo2.getTelefono());	
+	}
+
+	@Test
+	void testGetNIFCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.getNIF(),ejemplo2.getNIF());
+	}
+
+	@Test
+	void testSetNIFCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		ejemplo.setNIF("98215238B");
+		ejemplo2.setNIF("98215238B");
+		assertEquals(ejemplo.getNIF(),ejemplo2.getNIF());
+	}
+
+	@Test
+	void testGetDireccionCompletaCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.getDireccionCompleta(),ejemplo2.getDireccionCompleta());
+	}
+
+	@Test
+	void testSetDireccionCompletaCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		ejemplo.setDireccionCompleta("Juan Avila 12");
+		ejemplo2.setDireccionCompleta("Juan Avila 12");
+		assertEquals(ejemplo.getDireccionCompleta(),ejemplo2.getDireccionCompleta());
+	}
+
+	@Test
+	void testGetTelefonoCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.getTelefono(),ejemplo2.getTelefono());
+	}
+
+	@Test
+	void testSetTelefonoCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		ejemplo.setTelefono("817291863");
+		ejemplo2.setTelefono("817291863");
+		assertEquals(ejemplo.getTelefono(),ejemplo2.getTelefono());
+	}
+
+	@Test
+	void testGetNombreCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.getNombre(),ejemplo2.getNombre());
+	}
+
+	@Test
+	void testSetNombreCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		ejemplo.setNombre("Leandro");
+		ejemplo2.setNombre("Leandro");
+		assertEquals(ejemplo.getNombre(),ejemplo2.getNombre());
+	}
+
+	@Test
+	void testGetApellidosCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.getApellidos(),ejemplo2.getApellidos());
+	}
+
+	@Test
+	void testSetApellidosCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		ejemplo.setApellidos("Fernandez");
+		ejemplo2.setApellidos("Fernandez");
+		assertEquals(ejemplo.getApellidos(),ejemplo2.getApellidos());
+	}
+
+	@Test
+	void testGetEmailCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.getEmail(),ejemplo2.getEmail());
+	}
+
+	@Test
+	void testSetEmailCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		ejemplo.setEmail("LFernandez@gmail.com");
+		ejemplo2.setEmail("LFernandez@gmail.com");
+		assertEquals(ejemplo.getEmail(),ejemplo2.getEmail());
+	}
+
+	@Test
+	void testGetPasswordCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.getPassword(),ejemplo2.getPassword());
+	}
+
+	@Test
+	void testSetPasswordCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		ejemplo.setPassword("12445178Aa");
+		ejemplo2.setPassword("12445178Aa");
+		assertEquals(ejemplo.getPassword(),ejemplo2.getPassword());
+	}
+	
+	@Test
+	void testtoStringCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.toString(),ejemplo2.toString());
+		
+	}
+	
+	@Test
+	void testContraseniaSeguraCliente() {
+		Cliente ejemplo = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		Cliente ejemplo2 = new Cliente("Paco", "Corral", "FCorral@gmail.com", "8Caracteres", "8Caracteres", true, 5,
+				"12345678A", "Dirección", "666666666");
+		assertEquals(ejemplo.contraseniaSegura(ejemplo.getPassword()),ejemplo2.contraseniaSegura(ejemplo2.getPassword()));
+	
+	}
 
 }
